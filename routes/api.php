b8fc9d8d8d80a9ff8b0cc6c9a69b648697db63ce
Carolina <carolina.torres.baroffio@gmail.com>
2023-08-14 14:53:22 -0300

    rutas arregladas

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\API\PostController;
 
-Route::get('posts', [PostController::class, 'show']);
+Route::get('posts/{tematica?}', [PostController::class, 'VerPublicaciones']);
 Route::post('posts', [PostController::class, 'store']);
-
+Route::post('posts/{id}/comentarios', [PostController::class, 'comentarios']);
+Route::post('posts/{id}/like', [PostController::class, 'like']);
+Route::delete('posts/{id}', [PostController::class, 'eliminar']);
 
 
