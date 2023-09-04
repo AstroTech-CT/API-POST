579361b706a4508718c96df741b41c0bae22246c
Carolina <carolina.torres.baroffio@gmail.com>
2023-09-04 10:30:21 -0300

    cambio al nombre del controlador

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\API\PostController;
 
 Route::get('posts/{tematica?}', [PostController::class, 'VerPublicaciones']);
 Route::post('posts', [PostController::class, 'store']);
-Route::post('posts/{id}/comentarios', [PostController::class, 'comentarios']);
+Route::post('posts/{id}/comentarios', [PostController::class, 'Hacercomentario']);
 Route::post('posts/{id}/like', [PostController::class, 'like']);
 Route::delete('posts/{id}', [PostController::class, 'eliminar']);
 
